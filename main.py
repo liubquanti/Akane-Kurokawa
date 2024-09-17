a63cb739d371f232aa6432c49d4e008041797d03
Oleh Liubchenko <doroleijn@gmail.com>
2024-09-17 22:47:28 +0200

    ignore for repeat

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from telethon import TelegramClient, events, functions
 from characterai import aiocai
 from colorama import Fore
 from googletrans import Translator
+from fanblock import fans_ids
 
 client = TelegramClient('session_name', config.api_id, config.api_hash)
 characterai_client = aiocai.Client(config.charai_token)
+characterai_client_unk = aiocai.Client(config.charai_token_unk)
 
 previous_chat_id = config.previous_chat_id
 
@@ -25,8 +27,8 @@ async def get_character_ai_response(message_text):
         return response.text
     
 async def get_character_ai_response_denied(message_text):
-    me = await characterai_client.get_me()
-    async with await characterai_client.connect() as chat:
+    me = await characterai_client_unk.get_me()
+    async with await characterai_client_unk.connect() as chat:
         new_chat, answer = await chat.new_chat(config.char_id, me.id)
         response = await chat.send_message(config.char_id, new_chat.chat_id, message_text)
         return response.text
@@ -40,6 +42,13 @@ def update_config_file(key, value):
                 file.write(f"{key} = '{value}'\n")
             else:
                 file.write(line)
+
+def update_fans_ids_file(fans_ids):
+    with open('fanblock.py', 'w') as file:
+        file.write('fans_ids = [\n')
+        for fan_id in fans_ids:
+            file.write(f'    {fan_id},\n')
+        file.write(']\n')
 
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
@@ -64,35 +73,38 @@ async def handler(event):
         await asyncio.sleep(random.randint(1, 5))
         await client(functions.account.UpdateStatusRequest(offline=True))
     else:
-        await asyncio.sleep(random.randint(1, 5))
-        await client(functions.account.UpdateStatusRequest(offline=False))
-        await asyncio.sleep(random.randint(1, 3))
-        await event.message.mark_read()
-        print(f"{Fore.RED}[WRN] Користувач {event.sender_id} намагався написати!{Fore.RESET}")
-
-        message = event.message.text
-        print(f"{Fore.RED}[WRN] {event.sender_id}: {message}{Fore.RESET}")
-
-        translator = Translator()
-        message = event.message.text
-        translated_message = translator.translate(message, dest='en').text
-        print(f"{Fore.RED}[WRN] Повідомлення перекладено: {translated_message}{Fore.RESET}")
-        message = 'Imagine that a fan has written to you a message: "%s", but you don\'t want to communicate with them, write them a reply' % translated_message
-        await event.message.mark_read()
-
-        await asyncio.sleep(len(message) * 0.03 + 1)
-
-        response_text = await get_character_ai_response_denied(message)
-
-        async with client.action(event.chat_id, 'typing'):
-            await asyncio.sleep(len(response_text) * 0.1 + 1)
-
-        await event.reply(response_text)
-        print(f"{Fore.RED}[WRN] Akane: {response_text}{Fore.RESET}")
-
-        await asyncio.sleep(random.randint(1, 5))
-
-        await client(functions.account.UpdateStatusRequest(offline=True))
+        if event.sender_id in fans_ids:
+            await asyncio.sleep(random.randint(1, 5))
+            await client(functions.account.UpdateStatusRequest(offline=False))
+            await asyncio.sleep(random.randint(1, 5))
+            await event.message.mark_read()
+            print(f"{Fore.RED}[WRN] Було проігноровано користувача {event.sender_id}.{Fore.RESET}")
+            await asyncio.sleep(random.randint(1, 5))
+            await client(functions.account.UpdateStatusRequest(offline=True))
+        else:
+            await asyncio.sleep(random.randint(1, 5))
+            await client(functions.account.UpdateStatusRequest(offline=False))
+            await asyncio.sleep(random.randint(1, 3))
+            await event.message.mark_read()
+            fans_ids.append(event.sender_id)
+            update_fans_ids_file(fans_ids)
+            print(f"{Fore.RED}[WRN] Користувач {event.sender_id} намагався написати!{Fore.RESET}")
+            message = event.message.text
+            print(f"{Fore.RED}[WRN] {event.sender_id}: {message}{Fore.RESET}")
+            translator = Translator()
+            message = event.message.text
+            translated_message = translator.translate(message, dest='en').text
+            print(f"{Fore.RED}[WRN] Повідомлення перекладено: {translated_message}{Fore.RESET}")
+            message = 'Imagine that a fan has written to you a message: "%s", but you don\'t want to communicate with them, write a text for reply send me just a text of reply, nothing else' % translated_message
+            await event.message.mark_read()
+            await asyncio.sleep(len(message) * 0.03 + 1)
+            response_text = await get_character_ai_response_denied(message)
+            async with client.action(event.chat_id, 'typing'):
+                await asyncio.sleep(len(response_text) * 0.1 + 1)
+            await event.reply(response_text)
+            print(f"{Fore.RED}[WRN] Akane: {response_text}{Fore.RESET}")
+            await asyncio.sleep(random.randint(1, 5))
+            await client(functions.account.UpdateStatusRequest(offline=True))
 
 async def main():
     await client.start(config.phone_number)
